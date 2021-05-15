96e1910b64511b0a0ffd1b7fd19a1be266d00c1e
Vova <patutin.vovan@mail.ru>
2021-05-16 00:04:56 +0300

    save

--- methods/Lagrange.go
+++ methods/Lagrange.go
@@ -7,20 +7,20 @@ import (
 	"os"
 )
 
-type Lagrange struct {}
+type Lagrange struct{}
 
 func (Lagrange) Count(data *NewData) {
 	var up float32 = 1
 	var down float32 = 1
 	var res float32 = 0
-	for i:=0;i<data.N;i++{
-		for j:=0;j<data.N;j++{
-			if(i!=j){
+	for i := 0; i < data.N; i++ {
+		for j := 0; j < data.N; j++ {
+			if i != j {
 				up *= data.X - data.ArrayX[i]
 				down *= data.ArrayX[i] - data.ArrayX[j]
 			}
 		}
-		res += data.ArrayY[i] *up/down
+		res += data.ArrayY[i] * up / down
 	}
 	_, _ = fmt.Fprintf(os.Stdout, "Вычесленный Y методом Лагранжа: %d", res)
 	draw.DrawLagrange(data)
--- methods/Newton.go
+++ methods/Newton.go
@@ -7,4 +7,5 @@ import (
 type Newton struct {}
 
 func (Newton) Count(data *NewData) {
+
 }
